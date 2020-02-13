6e7207faf6bcaa11b778fb04ebe72d34b83ad48d
Ant <ryu4979@gmail.com>
2020-02-13 14:31:51 +0900

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <div class="container mt-3">
+      <form action="https://twitter.com/intent/tweet" method="get">
+        <legend>野球実況用ツイート画面</legend>
+        <div class="form-check">
+          <input
+            type="checkbox"
+            id="swallows"
+            value="#swallows"
+            v-model="checkedTags"
+            @change="tagUpdate"
+            class="form-check-input"
+          />
+          <label for="swallows">東京ヤクルトスワローズ</label>
+        </div>
+
+        <div class="form-check">
+          <input
+            type="checkbox"
+            id="tigers"
+            value="#tigers"
+            v-model="checkedTags"
+            @change="tagUpdate"
+            class="form-check-input"
+          />
+          <label for="tigers">阪神タイガース</label>
+        </div>
+
+        <div class="form-group">
+          <label for="tweet-textarea">Tweet本文</label>
+          <textarea v-model="tweet" id="tweet-textarea" name="text" class="form-control"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">送信</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import "@/css/bootstrap.min.css";
+
+export default {
+  name: "App",
+  data: function() {
+    return {
+      tweet: "テキスト",
+      checkedTags: [],
+      tagNames: ["#swallows", "#tigers"]
+    };
+  },
+  methods: {
+    tagUpdate: function() {
+      const tweet = this.tweet.slice();
+      const checkedTags = this.checkedTags.slice();
+      const tagNames = this.tagNames.slice();
+
+      // ツイート本文に記述するタグ一覧
+      const tags = checkedTags.join(" ");
+      // ツイート本文からハッシュタグを削除
+      const msg = tagNames
+        .reduce((acc, v) => acc.split(v).join(""), tweet)
+        .replace(/^\s+/,"");
+
+      this.tweet = tags + "\n" + msg;
+    }
+  }
+};
+</script>
