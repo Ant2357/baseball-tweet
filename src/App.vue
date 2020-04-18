aad809d0881c8907b5b81673758d815bda8b3676
Ant <ryu4979@gmail.com>
2020-04-18 19:20:05 +0900

    fix: checkboxにアニメーション付与

--- src/App.vue
+++ src/App.vue
@@ -53,6 +53,7 @@
 import "@/assets/css/bootstrap.min.css";
 import "@/assets/css/style.css";
 import "@/assets/css/text.css";
+import "@/assets/css/checkbox.css";
 import "@/assets/css/animate.min.css";
 
 import gsap from 'gsap';
