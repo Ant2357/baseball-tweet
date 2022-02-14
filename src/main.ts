f8e760ce3fcfc89b4669a66aa94e79fdd53ccafb
Ant <ryu4979@gmail.com>
2022-02-14 22:28:26 +0900

    fix: ライブラリの更新等 main.ts の型回りゆるして

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
+// @ts-ignore
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
+// @ts-ignore
+import { library, IconDefinition } from '@fortawesome/fontawesome-svg-core'
+// @ts-ignore
 import { faBlog, faFutbol } from '@fortawesome/free-solid-svg-icons'
+// @ts-ignore
 import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
+// @ts-ignore
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faBlog, faTwitter, faGithub, faFutbol)
+library.add(faBlog as IconDefinition,
+  faTwitter as IconDefinition,
+  faGithub as IconDefinition,
+  faFutbol as IconDefinition)
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
