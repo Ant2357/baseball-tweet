13dbcc1a7af90edb57e5a593bd0a3b2226884880
Ant2357 <ryu4979@gmail.com>
2022-04-06 02:48:51 +0900

    fix: 順位表のレイアウト実装(仮)

--- src/components/NpbStandings.vue
+++ src/components/NpbStandings.vue
@@ -1,14 +1,34 @@
 <template>
   <div>TODO: 順位表のレイアウト</div>
-  <p>{{ standings }} </p>
+
+  <vue-good-table
+    :columns="typeof standings[0] === 'undefined'
+      ? []
+      : Object.keys(standings[0]).map(labelName => {
+          return {
+            label: labelName,
+            field: labelName,
+            type: Number.isFinite(standings[0][labelName]) ? 'number' : 'string'
+          }
+        })"
+    :rows="standings"
+    theme="polar-bear"
+  />
 </template>
 
 <script lang="ts">
+// @ts-ignore
+import { VueGoodTable } from 'vue-good-table-next';
+import 'vue-good-table-next/dist/vue-good-table-next.css'
 import { defineComponent, PropType } from 'vue';
 
 export default defineComponent({
   props: {
-    standings: Object as PropType<{ [x: string]: string; }[]>
+    standings: Object as PropType<{ [x: string]: string; }[]>,
+    columns: Object as PropType<{ [x: string]: string; }[]>
+  },
+  components: {
+    VueGoodTable,
   },
   setup() {
     return {}
