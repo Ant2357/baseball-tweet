c88f7e25f9e11cbb4f582b15d066d628a06cc828
Ant2357 <ryu4979@gmail.com>
2023-07-25 19:56:20 +0900

    fix: FontAwesomeにて、使っていないfaFutbolをコードから削除

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import App from './App.vue'
 // @ts-ignore
 import { library, IconDefinition } from '@fortawesome/fontawesome-svg-core'
 // @ts-ignore
-import { faBlog, faFutbol } from '@fortawesome/free-solid-svg-icons'
+import { faBlog } from '@fortawesome/free-solid-svg-icons'
 // @ts-ignore
 import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
 // @ts-ignore
@@ -16,8 +16,7 @@ import VueScrollTo from 'vue-scrollto'
 
 library.add(faBlog as IconDefinition,
   faTwitter as IconDefinition,
-  faGithub as IconDefinition,
-  faFutbol as IconDefinition)
+  faGithub as IconDefinition)
 
 createApp(App)
   .use(VueScrollTo)
