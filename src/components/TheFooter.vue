caa8f20ce00bc18ff8d627dbb3e42a80b5b30331
Ant2357 <ryu4979@gmail.com>
2024-01-20 17:03:44 +0900

    chore: ライブラリの更新

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,7 +2,7 @@
   <footer class="footer has-text-white font-nicomoji">
     <div class="has-text-centered">
       <p>
-        © 2023 <a href="https://twitter.com/ant2357">ant2357</a>
+        © 2024 <a href="https://twitter.com/ant2357">ant2357</a>
       </p>
     </div>
   </footer>
